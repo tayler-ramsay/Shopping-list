0da047b1e6b1ce7c64a39fc856ccd5479fdf3eff
Tayler Ramsay <ramsay.tayler@gmail.com>
2018-03-20 09:50:25 -0400

    first commit

---
+++ src/components/ChangeColorBackgroundComponent.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <em>Change the list color</em>
+    <input v-model="bgColor" placeholder="Change Color">
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['value'],
+  methods: {
+    onInput (e) {
+      this.$emit('input', e.target.value)
+    }
+  }
+}
+</script>
+
+<style scoped>
+input {
+  margin-top: 2em;
+}
+</style>
---
+++ src/components/ShoppingListComponent.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <h2>{{ title }}</h2>
+    <add-item-component :id="id" @add="addItem"></add-item-component>
+    <items-component :items="items" :id="id"></items-component>
+    <div class="footer">
+      <hr />
+      <change-title-component :title="title" :id="id"></change-title-component>
+    </div>
+  </div>
+</template>
+
+<script>
+  import AddItemComponent from './AddItemComponent'
+  import ItemsComponent from './ItemsComponent'
+  import ChangeTitleComponent from './ChangeTitleComponent'
+  import ChangeColorBackgroundComponent from './ChangeColorBackgroundComponent'
+
+  export default {
+    components: {
+      AddItemComponent,
+      ItemsComponent,
+      ChangeTitleComponent,
+      ChangeColorBackgroundComponent
+    },
+    props: ['id', 'title', 'items'],
+    methods: {
+      addItem (text) {
+        this.items.push({
+          text: text,
+          checked: false
+        })
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .footer {
+    font-size: 1em;
+    margin-top: 20vh;
+  }
+</style>
